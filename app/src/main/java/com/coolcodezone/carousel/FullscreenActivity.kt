44a4228ae6da175a7511c4e2bc5869a8b9849637
dkiro <dkiro@stratusvideo.com>
2019-06-19 14:49:52 -0400

    added vibration and additional colours

--- app/src/main/java/com/coolcodezone/carousel/FullscreenActivity.kt
+++ app/src/main/java/com/coolcodezone/carousel/FullscreenActivity.kt
@@ -2,38 +2,45 @@ package com.coolcodezone.carousel
 
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.text.method.ScrollingMovementMethod
 import android.view.View
-import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SnapHelper
 import kotlinx.android.synthetic.main.activity_fullscreen.*
-import android.animation.ValueAnimator.AnimatorUpdateListener
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import kotlin.math.min
+import kotlin.math.roundToInt
+import android.os.Build
+import androidx.recyclerview.widget.SnapHelper
 
 
-class FullscreenActivity : AppCompatActivity(), OnSnapPositionChangeListener {
-    override fun onSnapPositionChange(position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class FullscreenActivity : AppCompatActivity(){
 
     private lateinit var stationsRecyclerAdapter: StationsRecyclerAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var snapHelper: SnapHelper
+
+    private lateinit var vibrator: Vibrator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_fullscreen)
 
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
         stationsRecyclerAdapter = StationsRecyclerAdapter()
         viewManager = CarouselLayoutManager(this)
 
-        val snapHelper = LinearSnapHelper()
+        snapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(stationsRecycler)
 
         stationsRecycler.apply {
@@ -41,14 +48,17 @@ class FullscreenActivity : AppCompatActivity(), OnSnapPositionChangeListener {
             layoutManager = viewManager
         }
 
+        var prevSelectedPage: Int = 0
         val snapPosChangeListener = object : OnSnapPositionChangeListener {
             override fun onSnapPositionChange(position: Int) {
-                val currentStation = stationsRecyclerAdapter.stationsList[position]
-                val currentStationView = snapHelper.findSnapView(viewManager)!!
-                val bgDrawable = currentStationView.background ?: stationsRecycler.background
-                val startColor = (bgDrawable as ColorDrawable).color
-                animateColorChange(startColor, currentStation.color, stationsRecycler)
+                if (position != prevSelectedPage) {
+                    resetOldView(prevSelectedPage)
+                }
 
+                updateSnappedView(position)
+
+                shakeItBaby()
+                prevSelectedPage = position
             }
 
         }
@@ -58,25 +68,16 @@ class FullscreenActivity : AppCompatActivity(), OnSnapPositionChangeListener {
             SnapOnScrollListener.Behavior.NOTIFY_ON_SCROLL,
             snapPosChangeListener
         )
-/*
-        var prevView: View? = null
-        stationsRecycler.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            val stationNameTextView = snapHelper.findSnapView(viewManager)
-                ?.findViewById<TextView>(R.id.stationNameTextView)
-            stationNameTextView?.isSelected = true
-            stationNameTextView?.textSize = 56.0f
-            stationNameTextView?.movementMethod = ScrollingMovementMethod()
+    }
 
-            if (prevView != null) {
-                val prevStationName = prevView!!.findViewById<TextView>(R.id.stationNameTextView)
-                prevStationName.isSelected = false
-                prevStationName.textSize = 48.0f
-                prevStationName.movementMethod = null
-            }
-
-            prevView = snapHelper.findSnapView(viewManager)
-        }*/
-
+    private fun updateSnappedView(position: Int) {
+        val currentStation = stationsRecyclerAdapter.stationsList[position]
+        val currentStationView = snapHelper.findSnapView(viewManager)!!
+        currentStationView.findViewById<TextView>(R.id.stationNameTextView)
+            .setTextColor(ContextCompat.getColor(applicationContext, R.color.activeStation))
+        val bgDrawable = currentStationView.background ?: stationsRecycler.background
+        val startColor = (bgDrawable as ColorDrawable).color
+        animateColorChange(startColor, currentStation.color, stationsRecycler)
     }
 
     fun animateColorChange(colorFrom: Int, colorTo: Int, view: View) {
@@ -85,9 +86,45 @@ class FullscreenActivity : AppCompatActivity(), OnSnapPositionChangeListener {
         colorAnimation.addUpdateListener {
             view.setBackgroundColor(it.animatedValue as Int)
             supportActionBar?.setBackgroundDrawable(ColorDrawable(it.animatedValue as Int))
-            it.startDelay = 300
+            window.statusBarColor = darkenColor(it.animatedValue as Int)
+            it.startDelay = 500
         }
         colorAnimation.start()
+    }
+
+    fun resetOldView(prevSelectedPage: Int) {
+        val preStationView = viewManager.findViewByPosition(prevSelectedPage)
+        preStationView?.findViewById<TextView>(R.id.stationNameTextView)
+            ?.setTextColor(
+                ContextCompat.getColor(
+                    applicationContext,
+                    R.color.inactiveStation
+                )
+            )
+    }
+
+    private fun darkenColor(color: Int): Int {
+        val factor = 0.8f
+        val a = Color.alpha(color)
+        val r = (Color.red(color) * factor).roundToInt()
+        val g = (Color.green(color) * factor).roundToInt()
+        val b = (Color.blue(color) * factor).roundToInt()
+        return Color.argb(
+            a,
+            min(r, 255),
+            min(g, 255),
+            min(b, 255)
+        )
+    }
+
+    private fun shakeItBaby() {
+        val length:Long = 30
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(
+                VibrationEffect.createOneShot(length, 10))
+        } else {
+            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(length)
+        }
     }
 }
 
